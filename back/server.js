fbeda0a507cc32b7f88243aa584f75ff97181a84
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-06-14 10:53:39 +0200

    dev: server with static files and directories

--- back/server.js
+++ back/server.js
@@ -1,15 +1,16 @@
 const express = require("express");
+const serveIndex = require("serve-index");
 const app = express();
 const port = 3000;
+const wwwDir = "./public";
 
 app.use((req, res, next) => {
   console.log("req.url", req.url);
   next();
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(express.static(wwwDir));
+app.use(serveIndex(wwwDir));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
