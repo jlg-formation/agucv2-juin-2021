164fb2a0ba62ebedd07baccc386983e16a99c298
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-06-14 10:38:10 +0200

    dev: write server "hello world"

--- back/server.js
+++ back/server.js
@@ -1,1 +1,16 @@
-console.log("coucou");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use((req, res, next) => {
+  console.log("req.url", req.url);
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
