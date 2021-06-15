1358494030f915d1b87558dbda8969903f5be9fd
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-06-15 16:31:34 +0200

    dev: added delete articles

--- back/server.ts
+++ back/server.ts
@@ -1,3 +1,4 @@
+import { Article } from "./../front/src/app/interfaces/article";
 import express from "express";
 import serveIndex from "serve-index";
 // import cors from "cors";
@@ -6,13 +7,17 @@ const app = express();
 const port = 3000;
 const wwwDir = "./public";
 
-const articles = [
-  { name: "Tournevis", price: 2.34, qty: 123 },
-  { name: "Tournevis Cruciforme", price: 4, qty: 12 },
-  { name: "Marteau", price: 1.2, qty: 20 },
+let articles: Article[] = [
+  { id: "a1", name: "Tournevis", price: 2.34, qty: 123 },
+  { id: "a2", name: "Tournevis Cruciforme", price: 4, qty: 12 },
+  { id: "a3", name: "Marteau", price: 1.2, qty: 20 },
 ];
 
+let articleSeq = 3;
+
 // app.use(cors({}));
+
+app.use(express.json());
 
 app.use((req, res, next) => {
   console.log("req.url", req.url);
@@ -21,6 +26,21 @@ app.use((req, res, next) => {
 
 app.get("/api/articles", (req, res) => {
   res.json(articles);
+});
+
+app.post("/api/articles", (req, res) => {
+  const article = req.body as Article;
+  articleSeq++;
+  article.id = "a" + articleSeq;
+  console.log("article: ", article);
+  articles.push(article);
+  res.json(article);
+});
+
+app.delete("/api/articles", (req, res) => {
+  const ids = req.body as string[];
+  articles = articles.filter((a) => !ids.includes(a.id));
+  res.status(204).end();
 });
 
 app.use(express.static(wwwDir));
--- front/src/app/services/http-article.service.ts
+++ front/src/app/services/http-article.service.ts
@@ -1,6 +1,6 @@
 import { ArticleService } from './article.service';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Article } from '../interfaces/article';
 
 @Injectable({
@@ -15,7 +15,46 @@ export class HttpArticleService extends ArticleService {
   refresh() {
     this.http.get<Article[]>('/api/articles').subscribe({
       next: (articles) => {
+        console.log('articles: ', articles);
         this.articles = articles;
+      },
+      complete: () => {
+        console.log('complete');
+      },
+      error: (err) => {
+        console.error('err: ', err);
+      },
+    });
+  }
+
+  add(article: Article) {
+    super.add(article);
+    this.http.post<void>('/api/articles', article).subscribe({
+      next: () => {
+        this.refresh();
+      },
+      complete: () => {
+        console.log('complete');
+      },
+      error: (err) => {
+        console.error('err: ', err);
+      },
+    });
+  }
+
+  remove(articles: Set<Article>) {
+    super.remove(articles);
+
+    const options = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+      body: [...articles].map((a) => a.id),
+    };
+
+    this.http.delete<void>('/api/articles', options).subscribe({
+      next: () => {
+        this.refresh();
       },
       complete: () => {
         console.log('complete');
