9b6584ffd7e5fda668989277f331b338c8ab0980
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-06-15 11:11:56 +0200

    dev: added loop with *ngFor + expression

--- front/src/app/stock/stock.component.ts
+++ front/src/app/stock/stock.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-stock',
   templateUrl: './stock.component.html',
-  styleUrls: ['./stock.component.scss']
+  styleUrls: ['./stock.component.scss'],
 })
 export class StockComponent implements OnInit {
+  articles = [
+    { name: 'Tournevis', price: 2.34, qty: 123 },
+    { name: 'Tournevis Cruciforme', price: 4, qty: 12 },
+    { name: 'Tournevis', price: 2.34, qty: 123 },
+    { name: 'Tournevis', price: 2.34, qty: 123 },
+    { name: 'Tournevis', price: 2.34, qty: 123 },
+    { name: 'Tournevis', price: 2.34, qty: 123 },
+    { name: 'Tournevis', price: 2.34, qty: 123 },
+    { name: 'Tournevis', price: 2.34, qty: 123 },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
