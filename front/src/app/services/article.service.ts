d21e9a5605b2af7f3fd8bae041259d3d13bb0446
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-06-15 12:07:46 +0200

    dev: localStorage to survive a refresh

--- front/src/app/services/article.service.ts
+++ front/src/app/services/article.service.ts
@@ -5,20 +5,27 @@ import { Article } from '../interfaces/article';
   providedIn: 'root',
 })
 export class ArticleService {
-  articles: Article[] = [
-    { name: 'Tournevis', price: 2.34, qty: 123 },
-    { name: 'Tournevis Cruciforme', price: 4, qty: 12 },
-    { name: 'Tournevis', price: 2.34, qty: 123 },
-    { name: 'Tournevis', price: 2.34, qty: 123 },
-    { name: 'Tournevis', price: 2.34, qty: 123 },
-    { name: 'Tournevis', price: 2.34, qty: 123 },
-    { name: 'Tournevis', price: 2.34, qty: 123 },
-    { name: 'Tournevis', price: 2.34, qty: 123 },
-  ];
+  articles: Article[] = this.getArticles();
 
   constructor() {}
 
+  getArticles(): Article[] {
+    const str = localStorage.getItem('articles');
+    if (!str) {
+      return [
+        { name: 'Tournevis', price: 2.34, qty: 123 },
+        { name: 'Tournevis Cruciforme', price: 4, qty: 12 },
+      ];
+    }
+    return JSON.parse(str);
+  }
+
+  save() {
+    localStorage.setItem('articles', JSON.stringify(this.articles));
+  }
+
   add(article: Article) {
     this.articles.push(article);
+    this.save();
   }
 }
