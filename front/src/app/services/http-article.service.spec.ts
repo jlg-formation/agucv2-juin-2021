02b0f70de35ddd8e4b99c68edf462007edef3617
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-06-15 16:51:47 +0200

    test: http article service simulation

--- front/src/app/services/http-article.service.spec.ts
+++ front/src/app/services/http-article.service.spec.ts
@@ -1,19 +1,41 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { HttpArticleService } from './http-article.service';
 
 describe('HttpArticleService', () => {
   let service: HttpArticleService;
+  let http: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
     });
     service = TestBed.inject(HttpArticleService);
+    http = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    http.verify();
   });
 
   it('should be created', () => {
+    const req = http.expectOne('/api/articles');
+    expect(req.request.method).toEqual('GET');
+    req.flush([]);
+    expect(service).toBeTruthy();
+  });
+
+  it('should be created with error', () => {
+    const req = http.expectOne('/api/articles');
+    expect(req.request.method).toEqual('GET');
+    req.flush('', {
+      status: 404,
+      statusText: 'Not Found',
+    });
     expect(service).toBeTruthy();
   });
 });
